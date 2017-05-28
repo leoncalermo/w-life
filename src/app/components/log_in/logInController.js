9d95cc29d0b5f2414770ea1c7bb00bcf803acf71
Natasha Martinelli <natasha.martinelli@wolox.com.ar>
2017-05-27 22:33:13 -0300

    intro view plus log_in img changes

--- src/app/components/log_in/logInController.js
+++ src/app/components/log_in/logInController.js
@@ -2,7 +2,7 @@ angular.module('app-bootstrap').controller('LogInController', [
   '$state',
   function($state) {
     this.requestLogIn = () => {
-      $state.go('principal.home');
+      $state.go('principal.intro');
     }
   }
 ]);
